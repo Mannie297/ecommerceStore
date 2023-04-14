1a545701765cc55bcb8876c6b1c838c2afe01ec2
mannie297 <dikson297@gmail.com>
2023-04-14 01:59:38 +0100

    svt commit

--- pages/success.js
+++ pages/success.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-//import { BsBagCheckFill} from 'react-icons/bs';
+import { BsBagCheckFill} from 'react-icons/bs';
 
 //import { useStateContext } from '@/context/StateContext';
 import { runFireworks } from '@/lib/util';
