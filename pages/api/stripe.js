f6fe94a7a9c0823f37ef57320b85bc42805b2692
mannie297 <dikson297@gmail.com>
2023-04-14 06:47:33 +0100

    egt commit

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -32,8 +32,8 @@ export default async function handler(req, res) {
 
       ],*/
       mode: 'payment',
-      success_url:"http://localhost:3000/success",
-      cancel_url:"http://localhost:3000/cancel",
+      success_url:`${req.headers.origin}/success`,
+      cancel_url:`${req.headers.origin}/cancel`,
   });
   res.send(JSON.stringify({
       url: session.url
