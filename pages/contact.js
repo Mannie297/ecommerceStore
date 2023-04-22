b0febbae80182881068f3bb34bf46be0e3522e1f
mannie297 <dikson297@gmail.com>
2023-04-22 06:37:49 +0100

    frtnt commit

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 import { sendContactForm } from '@/lib/sendContact';
-import React from 'react'
+import React, { useState } from 'react'
 //import { useState } from 'react';
 import { toast } from 'react-hot-toast';
 
